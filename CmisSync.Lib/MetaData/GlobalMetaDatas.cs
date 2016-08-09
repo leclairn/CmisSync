4e5eea37a9f422d8383df46a9f8638c34e440ad4
leclairn <nicolas.leclaire@phelma.grenoble-inp.fr>
2016-08-09 09:27:06 +0200

    Files Addes

---
+++ CmisSync.Lib/MetaData/GlobalMetaDatas.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace CmisSync.Lib.MetaData
+{
+    public class GlobalMetaDatas
+    {
+        public string typename;
+        public List<string> aspects;
+        public MetaDatas metadatas;
+
+
+        public GlobalMetaDatas()
+        {
+            typename = "";
+            aspects = new List<string>();
+            metadatas = new MetaDatas();
+        }
+
+        /// <summary>
+        /// Load metadatas from Xml File
+        /// </summary>
+        /// <param name="path">path to the Xml file </param>
+        public static GlobalMetaDatas Load(string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(GlobalMetaDatas));
+            StreamReader reader = new StreamReader(path);
+            GlobalMetaDatas metadatas = (GlobalMetaDatas)serializer.Deserialize(reader);
+            reader.Close();
+            return metadatas;
+        }
+
+        /// <summary>
+        /// Save metadatas to Xml File
+        /// </summary>
+        /// <param name="path">path to the Xml file </param>
+        public void Save(string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(GlobalMetaDatas));
+            StreamWriter writer = new StreamWriter(path);
+            serializer.Serialize(writer, this);
+            writer.Close();
+        }
+    }
+
+}
